f83ba4db8b3260cc2dd4c2835472466a0c23fec6
pinguinjkeke <yaavakov@gmail.com>
2018-02-04 20:37:46 +0300

    imports fixed

--- CustomDatePickerIOS.js
+++ CustomDatePickerIOS.js
@@ -1,28 +1,14 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * @providesModule DatePickerIOS
- * @flow
- *
- * This is a controlled component version of RCTDatePickerIOS
- */
-'use strict';
-
-const NativeMethodsMixin = require('NativeMethodsMixin');
-const React = require('React');
-const PropTypes = require('prop-types');
-const StyleSheet = require('StyleSheet');
-const View = require('View');
-const ViewPropTypes = require('ViewPropTypes');
-const ColorPropType = require('ColorPropType');
-
-const createReactClass = require('create-react-class');
-const requireNativeComponent = require('requireNativeComponent');
+import React from 'react';
+import {
+  ColorPropType,
+  NativeMethodsMixin,
+  requireNativeComponent,
+  StyleSheet,
+  View,
+  ViewPropTypes,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
 
 type DefaultProps = {
   mode: 'date' | 'time' | 'datetime',
